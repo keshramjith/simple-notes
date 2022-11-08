a58b9e41e74e2d133e89d9d9e411a4a18fb9afdc
Kesh Ramjith <kesh.ramjith@protonmail.com>
2022-11-08 13:09:07 +0200

    init backend

---
+++ sn-backend/src/main/java/com/example/snbackend/SnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.snbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnBackendApplication.class, args);
+	}
+
+}
